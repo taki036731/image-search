e138c03e241679705dd75064bce87f7729bfe98b
Nashi Namo <barefoot.036@gmail.com>
2025-09-24 05:08:10 +0000

    Dockerfileとビルドスクリプトを更新し、環境構築を改善 - Dockerfileにshfmtを追加 - devcontainer.jsonにshell-format拡張を追加 - .gitignoreにdist/app/を追加 - dist用のDockerfileを新規作成 - dist/build.shを新規作成し、プロジェクトビルド手順を追加

---
+++ dist/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+echo "Building the project..."
+
+if [ -d "app" ]; then
+	rm -rf app
+fi
+
+echo "Building the app..."
+mkdir app
+cd app || exit
+
+mkdir backend
+cp ../../backend/app.py backend/
+cp ../../backend/requirements.txt backend/
+cp ../../backend/.env backend/
+echo >> backend/requirements.txt
+echo "gunicorn" >> backend/requirements.txt
+
+mkdir frontend
+cp -r ../../frontend/dist frontend/

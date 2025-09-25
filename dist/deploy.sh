258a41b9b6d6526551645c5a63dfbbd2c13ad515
Nashi Namo <barefoot.036@gmail.com>
2025-09-25 07:15:44 +0000

    デプロイスクリプトを更新
    - アプリ名を「image-search」から「image-search-app」に変更
    - デプロイ地域を「asia-northeast1」から「asia-east1」に変更(Firebase authenticateに合わせるため)

--- dist/deploy.sh
+++ dist/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cd app && gcloud run deploy image-search \
+cd app && gcloud run deploy image-search-app \
 	--source . \
 	--platform managed \
-	--region asia-northeast1 \
+	--region asia-east1 \
 	--allow-unauthenticated \
 	--set-secrets GOOGLE_API_KEY=GOOGLE_API_KEY:latest,GOOGLE_CSE_ID=GOOGLE_CSE_ID:latest
